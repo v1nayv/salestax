c458dcb0b1f48d058ac3cd7b2baea2ceb851dc34
Vinay <vinay.vas@gmail.com>
2012-11-30 21:50:44 -0600

    moving these files under src/main/java.

--- src/test/java/com/lr/salestax/SalesTaxCalculationTestCase.java
+++ src/test/java/com/lr/salestax/SalesTaxCalculationTestCase.java
@@ -9,7 +9,7 @@ import org.junit.runners.JUnit4;
 /**
  * Description: Description goes here.
  * 
- * @author <a href="mailto:xxx@xxx">Vinay</a>
+ * @author <a href="mailto:xxx@xxx">Vinay Vasudevamurthy</a>
  * @version $Revision$ $Date$
  * @since 0.1
  */
