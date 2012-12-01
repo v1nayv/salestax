468c12167b7e0e4a1ee1b67e8f55667b0f1f728d
Vinay <vinay.vas@gmail.com>
2012-11-30 18:23:07 -0600

    first commit to command line sales tax application.

---
+++ src/main/java/com/lr/salestax/SalesTaxCalculation.java
@@ -0,0 +1,19 @@
+
+package com.lr.salestax;
+
+/**
+ * Description: Description goes here.
+ * 
+ * @author <a href="mailto:xxx@xxx">Vinay Vasudevamurthy</a>
+ * @version $Revision$ $Date$
+ * @since 0.1
+ */
+public class SalesTaxCalculation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
